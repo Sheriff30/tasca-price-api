85b38d5261b506df3e7d327273667612ba31d380
sherif <sherifdevv@gmail.com>
2025-06-21 21:52:30 +0300

    Tasca price API

---
+++ api/price.js
@@ -0,0 +1,18 @@
+const cheerio = require("cheerio");
+
+module.exports = async (req, res) => {
+  try {
+    const { gotScraping } = await import("got-scraping");
+    const url =
+      "https://www.tascaparts.com/oem-parts/ford-engine-timing-belt-tensioner-f1fz6c348c";
+    const { body } = await gotScraping(url);
+
+    const $ = cheerio.load(body);
+    const price = $("#product_price").text().trim();
+
+    res.status(200).json({ price });
+  } catch (err) {
+    console.error("❌ Error:", err);
+    res.status(500).json({ error: "Failed to fetch the price." });
+  }
+};
